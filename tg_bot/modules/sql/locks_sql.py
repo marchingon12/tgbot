f62d98fb10dadfe1cb81e8e62cfcbd50b34ffeb4
marchingon12 <augustthegreat.cool@gmail.com>
2021-02-09 12:40:07 +0100

    added more restriction types

--- tg_bot/modules/sql/locks_sql.py
+++ tg_bot/modules/sql/locks_sql.py
@@ -24,6 +24,10 @@ class Permissions(BASE):
     forward = Column(Boolean, default=False)
     game = Column(Boolean, default=False)
     location = Column(Boolean, default=False)
+    rtl = Column(Boolean, default=False)
+    button = Column(Boolean, default=False)
+    egame = Column(Boolean, default=False)
+    inline = Column(Boolean, default=False)
 
     def __init__(self, chat_id):
         self.chat_id = str(chat_id)  # ensure string
@@ -41,6 +45,10 @@ class Permissions(BASE):
         self.forward = False
         self.game = False
         self.location = False
+        self.rtl = False
+        self.button = False
+        self.egame = False
+        self.inline = False
 
     def __repr__(self):
         return "<Permissions for %s>" % self.chat_id
@@ -120,16 +128,24 @@ def update_lock(chat_id, lock_type, locked):
             curr_perm.sticker = locked
         elif lock_type == "gif":
             curr_perm.gif = locked
-        elif lock_type == 'url':
+        elif lock_type == "url":
             curr_perm.url = locked
-        elif lock_type == 'bots':
+        elif lock_type == "bots":
             curr_perm.bots = locked
-        elif lock_type == 'forward':
+        elif lock_type == "forward":
             curr_perm.forward = locked
-        elif lock_type == 'game':
+        elif lock_type == "game":
             curr_perm.game = locked
-        elif lock_type == 'location':
+        elif lock_type == "location":
             curr_perm.location = locked
+        elif lock_type == "rtl":
+            curr_perm.rtl = locked
+        elif lock_type == "button":
+            curr_perm.button = locked
+        elif lock_type == "egame":
+            curr_perm.egame = locked
+        elif lock_type == "inline":
+            curr_perm.inline = locked
 
         SESSION.add(curr_perm)
         SESSION.commit()
@@ -193,6 +209,14 @@ def is_locked(chat_id, lock_type):
         return curr_perm.game
     elif lock_type == "location":
         return curr_perm.location
+    elif lock_type == "rtl":
+        return curr_perm.rtl
+    elif lock_type == "button":
+        return curr_perm.button
+    elif lock_type == "egame":
+        return curr_perm.egame
+    elif lock_type == "inline":
+        return curr_perm.inline
 
 
 def is_restr_locked(chat_id, lock_type):
@@ -211,7 +235,12 @@ def is_restr_locked(chat_id, lock_type):
     elif lock_type == "previews":
         return curr_restr.preview
     elif lock_type == "all":
-        return curr_restr.messages and curr_restr.media and curr_restr.other and curr_restr.preview
+        return (
+            curr_restr.messages
+            and curr_restr.media
+            and curr_restr.other
+            and curr_restr.preview
+        )
 
 
 def get_locks(chat_id):
