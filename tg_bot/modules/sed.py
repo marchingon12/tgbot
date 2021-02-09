b93eb17210fa406e62a799c90f64a13359f674a3
marchingon12 <augustthegreat.cool@gmail.com>
2021-02-09 10:06:57 +0100

    improvements + more catch excp

--- tg_bot/modules/sed.py
+++ tg_bot/modules/sed.py
@@ -2,17 +2,34 @@ import re
 import sre_constants
 
 import telegram
-from telegram import Update, Bot
-from telegram.ext import run_async
+from telegram.ext import Filters
 
 from tg_bot import dispatcher, LOGGER
-from tg_bot.modules.disable import DisableAbleRegexHandler
+from tg_bot.modules.disable import DisableAbleMessageHandler
 
 DELIMITERS = ("/", ":", "|", "_")
 
 
+def infinite_checker(repl):
+    regex = [
+        r"\((.{1,}[\+\*]){1,}\)[\+\*].",
+        r"[\(\[].{1,}\{\d(,)?\}[\)\]]\{\d(,)?\}",
+        r"\(.{1,}\)\{.{1,}(,)?\}\(.*\)(\+|\* |\{.*\})",
+    ]
+    for match in regex:
+        status = re.search(match, repl)
+        if status:
+            return True
+        else:
+            return False
+
+
 def separate_sed(sed_string):
-    if len(sed_string) >= 3 and sed_string[1] in DELIMITERS and sed_string.count(sed_string[1]) >= 2:
+    if (
+        len(sed_string) >= 3
+        and sed_string[1] in DELIMITERS
+        and sed_string.count(sed_string[1]) >= 2
+    ):
         delim = sed_string[1]
         start = counter = 2
         while counter < len(sed_string):
@@ -31,8 +48,12 @@ def separate_sed(sed_string):
             return None
 
         while counter < len(sed_string):
-            if sed_string[counter] == "\\" and counter + 1 < len(sed_string) and sed_string[counter + 1] == delim:
-                sed_string = sed_string[:counter] + sed_string[counter + 1:]
+            if (
+                sed_string[counter] == "\\"
+                and counter + 1 < len(sed_string)
+                and sed_string[counter + 1] == delim
+            ):
+                sed_string = sed_string[:counter] + sed_string[counter + 1 :]
 
             elif sed_string[counter] == delim:
                 replace_with = sed_string[start:counter]
@@ -49,8 +70,7 @@ def separate_sed(sed_string):
         return replace, replace_with, flags.lower()
 
 
-@run_async
-def sed(bot: Bot, update: Update):
+def sed(update, context):
     sed_result = separate_sed(update.effective_message.text)
     if sed_result and update.effective_message.reply_to_message:
         if update.effective_message.reply_to_message.text:
@@ -63,24 +83,22 @@ def sed(bot: Bot, update: Update):
         repl, repl_with, flags = sed_result
 
         if not repl:
-            update.effective_message.reply_to_message.reply_text("You're trying to replace... "
-                                                                 "nothing with something?")
+            update.effective_message.reply_to_message.reply_text(
+                "You're trying to replace... " "nothing with something?"
+            )
             return
 
         try:
-            check = re.match(repl, to_fix, flags=re.IGNORECASE)
 
-            if check and check.group(0).lower() == to_fix.lower():
-                update.effective_message.reply_to_message.reply_text("Hey everyone, {} is trying to make "
-                                                                     "me say stuff I don't wanna "
-                                                                     "say!".format(update.effective_user.first_name))
-                return
+            # Protects bot from retarded geys -_-
+            if infinite_checker(repl) == True:
+                return update.effective_message.reply_text("Nice try -_-")
 
-            if 'i' in flags and 'g' in flags:
+            if "i" in flags and "g" in flags:
                 text = re.sub(repl, repl_with, to_fix, flags=re.I).strip()
-            elif 'i' in flags:
+            elif "i" in flags:
                 text = re.sub(repl, repl_with, to_fix, count=1, flags=re.I).strip()
-            elif 'g' in flags:
+            elif "g" in flags:
                 text = re.sub(repl, repl_with, to_fix).strip()
             else:
                 text = re.sub(repl, repl_with, to_fix, count=1).strip()
@@ -92,11 +110,15 @@ def sed(bot: Bot, update: Update):
 
         # empty string errors -_-
         if len(text) >= telegram.MAX_MESSAGE_LENGTH:
-            update.effective_message.reply_text("The result of the sed command was too long for \
-                                                 telegram!")
+            update.effective_message.reply_text(
+                "The result of the sed command was too long for \
+                                                 telegram!"
+            )
         elif text:
             update.effective_message.reply_to_message.reply_text(text)
 
+
+__mod_name__ = "Sed/Regex"
 
 __help__ = """
  - s/<text1>/<text2>(/<flag>): Reply to a message with this to perform a sed operation on that message, replacing all \
@@ -107,11 +129,14 @@ larger than {}.
 *Reminder:* Sed uses some special characters to make matching easier, such as these: `+*.?\\`
 If you want to use these characters, make sure you escape them!
 eg: \\?.
-""".format(telegram.MAX_MESSAGE_LENGTH)
+""".format(
+    telegram.MAX_MESSAGE_LENGTH
+)
 
-__mod_name__ = "Sed/Regex"
-
-
-SED_HANDLER = DisableAbleRegexHandler(r's([{}]).*?\1.*'.format("".join(DELIMITERS)), sed, friendly="sed")
+SED_HANDLER = DisableAbleMessageHandler(
+    Filters.regex(r"s([{}]).*?\1.*".format("".join(DELIMITERS))),
+    sed,
+    friendly="sed",
+)
 
 dispatcher.add_handler(SED_HANDLER)
